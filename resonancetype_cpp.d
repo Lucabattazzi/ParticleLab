fe9b3963ed5a1d8a059b7135de4ed8e4942ff9b4
Lucabattazzi <163546006+Lucabattazzi@users.noreply.github.com>
2024-12-14 13:20:24 +0100

    Delete resonancetype_cpp.d

--- resonancetype_cpp.d
+++
@@ -1,6 +0,0 @@
-
-# DO NOT DELETE
-
-./resonancetype_cpp.so: resonancetype.hpp particletype.hpp
-./resonancetype_cpp.so: /home/lucabattazzi/root/include/RVersion.h /home/lucabattazzi/root/include/ROOT/RConfig.hxx /home/lucabattazzi/root/include/TClass.h /home/lucabattazzi/root/include/TDictAttributeMap.h /home/lucabattazzi/root/include/TInterpreter.h /home/lucabattazzi/root/include/TROOT.h /home/lucabattazzi/root/include/TBuffer.h /home/lucabattazzi/root/include/TMemberInspector.h /home/lucabattazzi/root/include/TError.h /home/lucabattazzi/root/include/RtypesImp.h /home/lucabattazzi/root/include/TIsAProxy.h /home/lucabattazzi/root/include/TFileMergeInfo.h /home/lucabattazzi/root/include/TCollectionProxyInfo.h /home/lucabattazzi/root/bin/rootcling
-resonancetype_cpp__ROOTBUILDVERSION= 6.30/04
