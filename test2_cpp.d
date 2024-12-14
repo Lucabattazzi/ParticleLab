4d97074aa49e46af5cd06e8c1b016e0bef8e07f0
Lucabattazzi <163546006+Lucabattazzi@users.noreply.github.com>
2024-12-14 13:20:46 +0100

    Delete test2_cpp.d

--- test2_cpp.d
+++
@@ -1,6 +0,0 @@
-
-# DO NOT DELETE
-
-./test2_cpp.so: particle.hpp particletype.hpp resonancetype.hpp
-./test2_cpp.so: /home/lucabattazzi/root/include/RVersion.h /home/lucabattazzi/root/include/ROOT/RConfig.hxx /home/lucabattazzi/root/include/TClass.h /home/lucabattazzi/root/include/TDictAttributeMap.h /home/lucabattazzi/root/include/TInterpreter.h /home/lucabattazzi/root/include/TROOT.h /home/lucabattazzi/root/include/TBuffer.h /home/lucabattazzi/root/include/TMemberInspector.h /home/lucabattazzi/root/include/TError.h /home/lucabattazzi/root/include/RtypesImp.h /home/lucabattazzi/root/include/TIsAProxy.h /home/lucabattazzi/root/include/TFileMergeInfo.h /home/lucabattazzi/root/include/TCollectionProxyInfo.h /home/lucabattazzi/root/bin/rootcling
-test2_cpp__ROOTBUILDVERSION= 6.30/04
